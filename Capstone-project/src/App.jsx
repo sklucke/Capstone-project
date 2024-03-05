8f819f6f2ff26ec9ce2fb94561287c1ede03def7
sklucke <sklucke@sean>
2024-03-04 20:56:16 -0600

    feat: Login page

--- Capstone-project/src/App.jsx
+++ Capstone-project/src/App.jsx
@@ -1,13 +1,24 @@
+import {useState, useEffect } from "react";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import  HomePage from "./components/HomePage"
+import Login from "./components/Login";
 
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+    } else {
+      localStorage.removeItem("token");
+    }
+  }, [token]);
   return (
-    <div className="bg-purple-400">
+    <div>
       <h1>App</h1>
       <Routes>
+      <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
         
---
+++ Capstone-project/src/components/Login.jsx
@@ -0,0 +1,80 @@
+import {useState} from "react"
+import {useNavigate} from "react-router-dom"
+ import "./Login.css"
+
+
+
+const Login = ({ setToken }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const handleUsername = (event) => {
+      setUsername(event.target.value);
+    };
+  
+    const handlePassword = (event) => {
+      setPassword(event.target.value);
+    };
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/auth/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password,
+            }),
+          }
+        );
+        const result = await response.json();
+        setToken(result.token);
+      } catch (error) {
+        console.error("There was an error POST login", error);
+      }
+  
+      setUsername("");
+      setPassword("");
+      navigate("/");
+    };
+  
+    return (
+      <div className="login-container">
+        <form onSubmit={handleSubmit} className="form">
+          <h1>Please Login!</h1>
+          <div>
+            <br></br>
+            <label>
+              Username:
+              <input
+                type="text"
+                required
+                value={username}
+                onChange={handleUsername}
+              />
+            </label>
+          </div>
+          <br></br>
+          <div>
+            <label>
+              Password:{" "}
+              <input
+                type="password"
+                required
+                value={password}
+                onChange={handlePassword}
+              />
+            </label>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Login;
